6c6f4b40c22b0ec3f97d74b70b908e5f3b12020c
Granie Guillaume <guillaume.granie@continental-corporation.com>
2018-10-25 14:22:18 +0100

    wip

--- opencv_test.cpp
+++ opencv_test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
+#include <bitset>
 
 class DetectorExtractorMatcher
 {
@@ -19,7 +20,7 @@ public:
         static const int   cBriefPatchSize = 31; ///< Size of the image patch on which the descriptor is computed
         static const float cMaximumPatchSize = (cBriefPatchSize + 1U) * std::pow(cPyramidScaleFactor, static_cast<float>(cPyramidLevelCount + 1U)) + 1U; // Maximum size of a orb feature patch as if applied to level 0
 
-        static cv::Ptr<cv::ORB> orb = cv::ORB::create(cDefaultKeypointCount
+        static cv::Ptr<cv::ORB> orb = cv::ORB::create();/*cDefaultKeypointCount
                                                     , cPyramidScaleFactor
                                                     , cPyramidLevelCount
                                                     , cImageBorderThreshold
@@ -27,20 +28,22 @@ public:
                                                     , cBriefWta_k
                                                     , cKeypointScoreType
                                                     , cBriefPatchSize
-                                                    , cKeypointFastThreshold);
+                                                    , cKeypointFastThreshold);*/
 
-        orb->detect(image, keypoints[1]);
-        kps = keypoints[1];
+        orb->detectAndCompute(image, cv::Mat(), keypoints, descriptors[1]);
+        kps = keypoints;
     }
 
     void matchFeatures()
     {
+        if (descriptors[0].size() == 0) std::copy(descriptors[1].begin(), descriptors[1].end(), descriptors[0].begin());
 
     }
 
 private:
 
-    std::vector<cv::KeyPoint> keypoints[2];
+    std::vector<cv::KeyPoint> keypoints;
+    std::vector<unsigned char> descriptors[2];
     std::vector<cv::DMatch>   matches;
     cv::BFMatcher matcher = cv::BFMatcher(cv::NORM_HAMMING);
 };
@@ -48,7 +51,6 @@ private:
 int main()
 {
 	cv::TermCriteria termcrit(cv::TermCriteria::COUNT | cv::TermCriteria::EPS, 20, 0.03);
-	cv::Size winSize(200, 300);
     bool SET_CAMERA = false;
     // Open vid reader
     int delay = 20;
@@ -72,6 +74,10 @@ int main()
             break;
         }
 
+        cv::Mat gray;
+
+        //cv::cvtColor(image, gray, CV_BGR2GRAY);
+
         dem.extractFeatures(image, kps);
 
         for (auto& kp : kps)
@@ -79,10 +85,10 @@ int main()
             cv::circle(image, kp.pt, 2, cv::Scalar(0, 0, 255));
         }
 
-		cv::imshow("frame", image);
-		if (cv::waitKey(delay) >= 0) {
-			break;
-		}
+        cv::imshow("frame", image);
+        if (cv::waitKey(delay) >= 0) {
+            break;
+        }
 
     }
     capture.release();
